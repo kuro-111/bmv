654e187ae8369a10ef2f67f92a3cb76402ddf56a
kuro-111 <graceyann666@gmail.com>
2025-02-13 09:21:19 +0800

    fix design and finish button functionsgit status

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-const popButton = document.querySelector("div.popUp");
+const popButton = document.querySelector(".button1");
+const popWindow = document.querySelector(".popup");
+
+console.log(popButton);
+
+popButton.addEventListener(
+    "click", (e) => {
+        setTimeout(() => {
+            popWindow.classList.add("show");
+            
+        }, 1000);
+
+
+    console.log("I am here and I am working!!")
+} );
+
+
